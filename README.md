fd13848c4d5980347b402b6e43aafd40f844e134
StaniD <StaniD@users.noreply.github.com>
2017-08-08 19:53:10 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,10 +1,8 @@
-# get_clean_data_project
-Getting and Cleaning Data
-
+## Getting and Cleaning Data
 The purpose of this project is to collect, work with, and clean a data set. The goal is to prepare tidy data that can be used for later analysis. 
 The row data: a non-tidy dataset of accelerometers data from the Samsung Galaxy S smartphone (see: http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones).
 
-Instructions: 
+## Instructions: 
 Create one R script called run_analysis.R that does the following:
     1. Merges the training and the test sets to create one data set
     2. Extracts only the measurements on the mean and standard deviation for each measurement
@@ -12,25 +10,28 @@ Create one R script called run_analysis.R that does the following:
     4. Appropriately labels the data set with descriptive activity names
     5. Creates a second, independent tidy data set with the average of each variable for each activity and each subject
     
-Content in this repository: 
+## Content in this repository: 
     1. run_analysis.R: R script to merge, clean-up, transform the raw data and create the tiny data set (created in RStudio Ver. 1.0.153 in Windows 10)
     2. CodeBook.md: Code Book describes the original data, the variables and the transformations performed to clean up the data and creates tidy dataset
-    3. outputdata.txt: Tidy dataset output file created from the script run_analysis.R
-    4. README.md 
+    3. alltidydata.txt: All tidy dataset after step 4 created with run_analysis.R
+    4. tidymeansdata.txt: Second tidy dataset with means created from step 5 with thescript run_analysis.R
+    5. README.md 
     
-Before running the script: 
+## Before running the script: 
     1. Read the Codebook for the steps taken and variables info
     2. Ensure that you have installed "data.table" and "dplyr" packages
     3. To install packages use: install.packages("data.table"); install.packages("dplyr")    
 
-During run of the script: 
+## During run of the script: 
     1. Opens "README.txt"" to view and refer info for the dataset files.
-    2. Opens "features_info.txt" to refer more info about variables measurement names 
-    3. Opens "outputdata.txt - the independent tidy dataset output file with average of the measurements
+    2. Opens "features_info.txt" to refer more info about measurement variables names 
+    3. Opens "tidymeansdata.txt" - the 2nd tidy dataset file with average of the measurements
     
-Run run_analysis.R
-    
-Notes: 
-    the row data will be downloaded in your working directory. 
-    The second tidy dataset "outputdata.txt" is saved in the unzipped folder: "UCI HAR Dataset" 
-    Variables names and data structure - refer to Codebook.md in this repo.
+## Run run_analysis.R
+    source(run_analysis.R)
+## Notes: 
+    The raw data will be downloaded in your working directory. 
+    2 files with datasets will be saved in your working directory:
+    - all tidy dataset as "alltidydata.txt"
+    - second tidy dataset as "tidymeansdata.txt"
+    Variables names and data structure - refer to CodeBook.md in this repo. 
---
+++ run_analysis.R
@@ -0,0 +1,80 @@
+wd<-getwd()
+#Download the Dataset.zip
+if(file.exists("Dataset.zip")==FALSE){
+  download.file(url="https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip" ,destfile="Dataset.zip")
+}
+# unzip the dataset
+unzip("Dataset.zip")
+if(dir.exists("UCI HAR Dataset") == FALSE){
+  stop("downloaded zip is corrupt. download and extract manually in working directory")
+  }
+
+# set working dir to "UCI HAR Dataset"
+setwd("UCI HAR Dataset")
+
+# load libraries 
+library(data.table)
+library(dplyr)
+# refer to the readme file for more info on test and training data files 
+file.show("README.txt")
+
+#1. Merge the training and the test sets to create one data set
+    #Read the testing and training dataset, labels and subjects data
+dataset_train <- read.table("./train/X_train.txt")
+activitylabels_train <- read.table("./train/y_train.txt")
+subjects_train <- read.table("./train/subject_train.txt")
+dataset_test <- read.table("./test/X_test.txt")
+activitylabels_test <- read.table("./test/y_test.txt") 
+subjects_test <- read.table("./test/subject_test.txt")
+
+    # merge the test end training data by row
+alldataset <- rbind(dataset_train, dataset_test)
+allactivities <- rbind(activitylabels_train, activitylabels_test)
+allsubjects <- rbind(subjects_train, subjects_test) 
+    # read in the measurement labels dataset
+allfeatures <- read.table("features.txt")
+file.show("features_info.txt")
+    # names the columns of alldataset with 2nd column of allfeatures as vector 
+colnames(alldataset) <- as.vector(allfeatures$V2)
+
+#2. Extracts only the measurements on the mean and standard deviation for each measurement
+    # subset only the columns that contain "mean()" or "std()"
+alldataset <- alldataset[, grepl("mean\\(\\)|std\\(\\)", colnames(alldataset))] 
+
+#3. Uses descriptive activity names to name the activities in the data set 
+activity_names <- read.table("activity_labels.txt")
+colnames(allactivities)<- c("activity_num")
+    # transform activity_names - to lower case: 
+activity_names$V2<- tolower(activity_names$V2)
+activity<-as.vector(activity_names[match(allactivities$activity_num, activity_names$V1), "V2"])
+
+    # merge as first column "ativity" into alldataset
+alldataset<-cbind(activity,alldataset)
+    # names V1 and merge as fisrt column allsubjects into alldataset
+colnames(allsubjects)<- c("subjects")
+alldataset<-cbind(allsubjects,alldataset)
+ 
+#4. Appropriately labels the dataset with descriptive activity names. 
+    # clean the data variables: remove "(", ")", "-" from the column names 
+    # repetition words "bodybody" to "body"
+    # replase t to time and f to frequency
+alldataset <- alldataset %>% 
+  setNames(tolower(gsub("\\(\\)","",names(.)))) %>%                                       
+  setNames(gsub("-","_", names(.))) %>% 
+  setNames(gsub("bodybody","body", names(.))) %>%
+  setNames(gsub("^t", "time", names(.))) %>%
+  setNames(gsub("^f", "frequency", names(.)))
+
+#5. Creates a second, independent tidy data set with the average of each variable for each activity and each subject. 
+  # group the running data by subject and activity, then
+  # calculate the mean of every measurement.
+outputdata <- alldataset %>%
+  group_by(subjects, activity) %>%
+  summarise_all(funs(mean))
+
+# write alltidydata and tidymeansdata with calculated means of measurements 
+# to 2 files in your working directory
+setwd(wd)
+write.table(alldataset, file="alltidydata.txt", row.name=FALSE)
+write.table(outputdata, file="tidymeansdata.txt", row.name=FALSE)
+file.show("tidymeansdata.txt")
